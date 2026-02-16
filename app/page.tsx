d0f8a423a06350287416d7ec257d749ae5c0d087
Numbers analyst <mrgregorybrzeczyszczykiewicz@proton.me>
2026-02-16 01:55:38 +0100

    Enable loader

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ import { Footer } from "@/components/Footer";
 export default function Home() {
   return (
     <>
-      {/* <Loader /> */}
+      <Loader />
       <Hero />
       <Contrast />
       <Consequences />
